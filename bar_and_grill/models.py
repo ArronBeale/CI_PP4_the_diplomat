13cfd75666fd6885ac6b1c239b6af7df243b7491
Arron <beale86@msn.com>
2022-08-17 19:51:35 +0000

    Add food item and drink item models to models.py

--- bar_and_grill/models.py
+++ bar_and_grill/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+FOOD_TYPE = ((0, 'Starters'), (1, 'Mains'), (2, 'Desserts'), (3, 'New'))
+DRINK_TYPE = ((0, 'Wines'), (1, 'Beers'), (2, 'Cocktails'), (3, 'New'))
+
+# Models for Food and Drinks items
+Class FoodItem(models.Model):
+    """
+    Food item model
+    """
+    food_id = models.AutoField(primary_key=True)
+    food_name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=200, unique=True)
+    price = models.FloatField()
+    food_type = models.IntegerField(choices=FOOD_TYPE, default=3)
+    available = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-available']
+
+    def __str__(self):
+        return self.food_name
+
+
+class DrinkItem(models.Model):
+    """
+    Drink item model
+    """
+    drink_id = models.AutoField(primary_key=True)
+    drink_name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=200, unique=True)
+    price = models.FloatField()
+    drink_type = models.IntegerField(choices=DRINK_TYPE, default=3)
+    available = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-available']
+
+    def __str__(self):
+        return self.drink_name
