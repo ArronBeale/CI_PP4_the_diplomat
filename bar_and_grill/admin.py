a4148658a7440a379add98869b0641bbe7aa64f0
Arron <beale86@msn.com>
2022-08-17 20:56:58 +0000

    Add list filter to FoodAdmin and DrinkAdmin in admin.py

--- bar_and_grill/admin.py
+++ bar_and_grill/admin.py
@@ -5,9 +5,11 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(FoodItem)
 class FoodAdmin(SummernoteModelAdmin):
+    list_filter = ('available', 'food_type')
     summernote_fields = ('description')
 
 
 @admin.register(DrinkItem)
 class DrinkAdmin(SummernoteModelAdmin):
+    list_filter = ('available', 'drink_type')
     summernote_fields = ('description')
