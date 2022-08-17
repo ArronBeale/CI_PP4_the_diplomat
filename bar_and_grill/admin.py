fe93915f8acc43cd630db48d9f818f6915baba51
Arron <beale86@msn.com>
2022-08-17 21:04:09 +0000

    Add list display and search field for FoodAdmin and DrinkAdmin in admin.py

--- bar_and_grill/admin.py
+++ bar_and_grill/admin.py
@@ -5,11 +5,15 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(FoodItem)
 class FoodAdmin(SummernoteModelAdmin):
+    list_display = ('food_name', 'food_type', 'price', 'available')
+    search_fields = ('food_name', 'description')
     list_filter = ('available', 'food_type')
     summernote_fields = ('description')
 
 
 @admin.register(DrinkItem)
 class DrinkAdmin(SummernoteModelAdmin):
+    list_display = ('drink_name', 'drink_type', 'price', 'available')
+    search_fields = ('drink_name', 'description')
     list_filter = ('available', 'drink_type')
     summernote_fields = ('description')
