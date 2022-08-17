700e18c6966a86dc06f638e7ac7e41b1f61bb299
Arron <beale86@msn.com>
2022-08-17 20:24:50 +0000

    Change summernote fields to description to match food and drink model

--- bar_and_grill/admin.py
+++ bar_and_grill/admin.py
@@ -3,11 +3,11 @@ from .models import FoodItem, DrinkItem
 from django_summernote.admin import SummernoteModelAdmin
 
 
-@admin.register(FoodItem())
+@admin.register(FoodItem)
 class FoodAdmin(SummernoteModelAdmin):
-    summernote_fields = ('content')
+    summernote_fields = ('description')
 
 
-@admin.register(DrinkItem())
+@admin.register(DrinkItem)
 class DrinkAdmin(SummernoteModelAdmin):
-    summernote_fields = ('content')
+    summernote_fields = ('description')
