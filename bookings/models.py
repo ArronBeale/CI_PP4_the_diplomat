00e2db45fd3d5c0827b45a7286b534159f4ab29a
Arron <beale86@msn.com>
2022-08-18 18:19:09 +0000

    Add class for booking model to models.py of bookings app

--- bookings/models.py
+++ bookings/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+time_slots = (
+
+)
+
+
+
 
 class Table(models.Model):
     """
@@ -15,3 +21,29 @@ class Table(models.Model):
 
     def __str__(self):
         return self.table_name
+
+
+class Booking(models.Model):
+    """
+    a class for the booking model
+    """
+    booking_id = models.AutoField(primary_key=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    requested_date = models.DateField()
+    requested_time = models.CharField(
+        max_length=25, choices=time_slots, default='17:00')
+    table = models.ForeignKey(
+        Table, on_delete=models.CASCADE, related_name="table_reserved",
+        null=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user", null=True)
+    status = models.CharField(max_length=25, unique=True)
+
+    class Meta:
+        ordering = ['-requested_time']
+
+    def __str__(self):
+        return self.booking_id
+
+
+class User(models.Model):
