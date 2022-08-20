f85bc8591aa144e63192a1de8508a6be78a13872
Arron <beale86@msn.com>
2022-08-20 12:52:50 +0000

    Add FoodList and DrinkList to views.py in bar and grill app

--- bar_and_grill/views.py
+++ bar_and_grill/views.py
@@ -1,3 +1,29 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.shortcuts import render
+from django.views import generic
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Internal:
+from .models import FoodItem, DrinkItem
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Create your views here.
+
+class FoodList(generic.ListView):
+    """
+    This is the view for all food items available on
+    the food menu
+    """
+    model = FoodItem
+    queryset = FoodItem.objects.filter(available=1).order_by('-food_name')
+    template_name = 'menu.html'
+
+
+class DrinkList(generic.ListView):
+    """
+    This is the view for all drink items available on
+    the drink menu
+    """
+    model = DrinkItem
+    queryset = DrinkItem.objects.filter(available=1).order_by('-drink_name')
+    template_name = 'menu.html'
