e40adceccff86b49d67a3b36b6b87dcba2908285
Arron <beale86@msn.com>
2022-09-18 15:04:44 +0000

    Add CommentAdmin class to admin.py of blog app

--- blog/admin.py
+++ blog/admin.py
@@ -11,3 +11,15 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_date')
     list_display = ('title', 'slug', 'status', 'created_date')
     search_fields = ['title', 'content']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_filter = ('approved', 'created_date')
+    list_display = ('name', 'body', 'post', 'created_date', 'approved')
+    search_fields = ['name', 'email', 'body']
+    actions = ['approve_comments']
+
+    def approve_comments(self,request, queryset):
+        queryset.update(approved=True)
