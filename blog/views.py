5896b85d8cb8f69caa88994a953a2fb92b243224
Arron <beale86@msn.com>
2022-09-19 19:31:09 +0000

    Add script.js to static, link script.js to base.html, adjust layout of blog posts in blog.html, adjust layout to blog_expand.html, add code to only show published posts in blog.html

--- blog/views.py
+++ blog/views.py
@@ -14,7 +14,7 @@ class PublishedPosts(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_date')
     template_name = 'blog.html'
-    paginated_by = 6
+    paginated_by = 4
 
     def get(self, request, *args, **kwargs):
         """
