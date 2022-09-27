92c609c16ea9de817f7e1ffc3317b596f960d8d2
Arron <beale86@msn.com>
2022-09-27 18:42:25 +0000

    Add code to switch to test databse in settings.py for tesing, add super user to test database, adjust blog comment format in blog_expanded.html

--- the_diplomat/settings.py
+++ the_diplomat/settings.py
@@ -103,16 +103,20 @@ WSGI_APPLICATION = 'the_diplomat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+if os.environ.get("DEVELOPMENT") == "True":
+    # Testing Database
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    # Heroku Database
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
+        }
 
 
 # Password validation
