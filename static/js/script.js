210071fd6d586bb5f2c7c6980aca2af10fb0aa22
Arron <beale86@msn.com>
2022-10-01 15:57:42 +0000

    Add validation images to docs folder

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,5 @@
 // Close messages after 3 seconds
 $(document).ready(function () {
-
     setTimeout(function () {
         let messages = document.getElementById('msg');
         let alert = new bootstrap.Alert(messages);
