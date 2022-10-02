69dd7b2221a896ac45e88cfb5aa3664bb496ba45
Arron <beale86@msn.com>
2022-10-02 14:15:40 +0000

    Clean code according to pep8

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 // Close messages after 3 seconds
 $(document).ready(function () {
     setTimeout(function () {
-        let messages = document.getElementById('msg');
+        let messages = document.getElementById("msg");
         let alert = new bootstrap.Alert(messages);
         alert.close();
     }, 3000);
--- the_diplomat/settings.py
+++ the_diplomat/settings.py
@@ -2,8 +2,6 @@ from pathlib import Path
 import os
 import dj_database_url
 from django.contrib.messages import constants as messages
-if os.path.isfile('env.py'):
-    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
