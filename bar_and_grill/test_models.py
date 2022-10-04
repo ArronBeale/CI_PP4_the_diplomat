6088ca9c961f72f1be362d02741f4195cde4f54a
Arron <beale86@msn.com>
2022-10-04 17:21:02 +0000

    Add auto tests for food and drinks model to test_models.py in bar and grill app

--- bar_and_grill/test_models.py
+++ bar_and_grill/test_models.py
@@ -1,6 +1,33 @@
 # Imports
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 3rd party:
+from django.test import TestCase
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Internal:
+from .models import FoodItem, DrinkItem
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class TestModels(TestCase):
+
+    def test_new_foods(self):
+        food = FoodItem.objects.create(
+            food_name='Test Food',
+            description='Test description',
+            price='9.99',
+            food_type=1
+            )
+        self.assertFalse(food.available)
+        self.assertEqual(food.price, '9.99')
+        self.assertEqual(food.description, 'Test description')
+
+    def test_new_drinks(self):
+        drink = DrinkItem.objects.create(
+            drink_name='Test drink',
+            description='Test description',
+            price='9.99',
+            drink_type=1
+            )
+        self.assertFalse(drink.available)
+        self.assertEqual(drink.price, '9.99')
+        self.assertEqual(drink.description, 'Test description')
