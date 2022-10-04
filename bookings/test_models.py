21969f87f97a9495966f538a8f5573fc495492a3
Arron <beale86@msn.com>
2022-10-04 18:55:22 +0000

    Add test_creste_booking to test_models.py in bookings app

--- bookings/test_models.py
+++ bookings/test_models.py
@@ -34,5 +34,13 @@ class TestModels(TestCase):
             email='test@aaa.com',
             )
 
-    def test_create_booking(self):
+    def test_booking(self):
         self.assertEqual(self.booking.name, 'Tester Test')
+        self.assertEqual(self.booking.booking_id, 32)
+        self.assertEqual(self.booking.requested_date, '2022-12-12')
+        self.assertEqual(self.booking.requested_time, '12:00')
+        self.assertEqual(self.booking.guest_count, 6)
+        self.assertEqual(self.booking.status, 'awaiting confirmation')
+        self.assertEqual(self.booking.name, 'Tester Test')
+        self.assertEqual(self.booking.phone, '+353123456789')
+        self.assertEqual(self.booking.email, 'test@aaa.com')
